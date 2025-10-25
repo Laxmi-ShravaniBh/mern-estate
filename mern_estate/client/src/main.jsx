56c9e14ca1dff3c371a79d488cc8e11d307270e0
Laxmi-ShravaniBh <laxmishravanibh@outlook.com>
2025-10-25 13:38:30 +0530

    Add Redux persist

--- mern_estate/client/src/main.jsx
+++ mern_estate/client/src/main.jsx
@@ -1,22 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const container = document.getElementById('root')
 
-if (container) {
-  const root = createRoot(container)
-
-  root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
-}
+);
