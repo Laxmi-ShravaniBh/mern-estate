5bf3ac8f81be279d0f15cac44932e51a10904464
Laxmi-ShravaniBh <laxmishravanibh@outlook.com>
2025-11-09 22:33:34 +0530

    Create Update Listing API Route

--- mern_estate/api/controllers/listing.controller.js
+++ mern_estate/api/controllers/listing.controller.js
@@ -1,4 +1,6 @@
 import Listing from "../models/listing.model.js";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -26,3 +28,25 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return res.status(404).json({ message: "Listing not found" });
+        }
+
+        if (req.user.id !== listing.userRef) {
+            return res.status(401).json({ message: "You are not authorized to update this listing" });
+        }
+
+        try {
+            const updatedListing = await Listing.findByIdAndUpdate(
+                req.params.id, req.body, {new: true});
+        return res.status(200).json(updatedListing);
+        } catch (error) {
+            next(error);
+        }
+
+    };
+
+
