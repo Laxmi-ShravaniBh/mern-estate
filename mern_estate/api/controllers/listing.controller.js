209ae5cfefc0eddc52ca5642d547f6771099b02c
Laxmi-ShravaniBh <laxmishravanibh@outlook.com>
2025-11-12 00:02:28 +0530

    Create Search APIRoute

--- mern_estate/api/controllers/listing.controller.js
+++ mern_estate/api/controllers/listing.controller.js
@@ -61,4 +61,59 @@ export const getListing = async (req, res, next) => {
     }
 };
 
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 10;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        
+        let offer = req.query.offer;
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        } else if (offer === 'true') {
+            offer = true;
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        } else if (furnished === 'true') {
+            furnished = true;
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        } else if (parking === 'true') {
+            parking = true;
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            ...(type && { type }),
+            ...(offer && { offer }),
+            ...(furnished && { furnished }),
+            ...(parking && { parking }),
+        })
+        .sort({ [sort]: order })
+        .skip(startIndex)
+        .limit(limit);
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
 
